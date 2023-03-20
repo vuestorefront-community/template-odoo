ee7a11440929930fcfdce00f7388dc702f3ff750
Leonardo Muniz <lmuniz@odoogap.com>
2023-03-20 17:23:12 -0300

    Removed config to expose local server as ip

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,10 +10,6 @@ const { combine, timestamp, label, prettyPrint } = format;
 
 const isDev = process.env.NODE_ENV !== 'production';
 export default {
-  server: {
-    port: 3000,
-    host: '0.0.0.0'
-  },
   hooks: {
     build: {
       before() {
