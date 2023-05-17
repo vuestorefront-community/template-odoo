8bd2a47ea1720cd9f3437ec8770c0867350a68d3
ekhan <ekhan@odoogap.com>
2023-05-17 20:27:30 +0600

    16037

--- components/MyAccount/ShippingList.vue
+++ components/MyAccount/ShippingList.vue
@@ -64,7 +64,7 @@ export default defineComponent({
   props: {
     addresses: {
       type: Array as PropType<Address[]>,
-      default: {}
+      default: []
     }
   },
   setup({}, { emit }) {
