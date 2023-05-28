0a4d6047f3a3b60f151b5a1119280d2da57da96c
ekhan <ekhan@odoogap.com>
2023-05-28 16:17:41 +0600

    load-data

--- middleware/checkout.js
+++ middleware/checkout.js
@@ -1,13 +1,13 @@
 const canEnterShipping = (cart) => cart?.order.orderLines?.length > 0 || false;
 
 const canEnterBiling = (cart) => {
-  const checkShippingAddress = cart?.order.partnerShipping
-  return canEnterShipping(cart) && checkShippingAddress.id && checkShippingAddress.street && checkShippingAddress.country
+  const checkShippingAddress = cart?.order.partnerShipping.id && cart?.order.partnerShipping.street
+  return canEnterShipping(cart) && checkShippingAddress
 };
 
 const canEnterPayment = (cart) => {
-const checkInvoiceAddress = cart?.order.partnerInvoice
-  return canEnterShipping(cart) && canEnterBiling(cart) && checkInvoiceAddress.id && checkInvoiceAddress.street && checkInvoiceAddress.country;
+const checkInvoiceAddress = cart?.order.partnerInvoice.id && cart?.order.partnerInvoice.street
+  return canEnterShipping(cart) && canEnterBiling(cart) && checkInvoiceAddress;
 } 
 
 export default async ({ app, $vsf, redirect }) => {
