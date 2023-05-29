67c92d8782c95d8b533853ff267dcd66a1031fd7
ekhan <ekhan@odoogap.com>
2023-05-29 12:49:20 +0600

    16195

--- components/Category/Navbar.vue
+++ components/Category/Navbar.vue
@@ -31,7 +31,6 @@
             :placeholder="$t('Select sorting')"
             data-cy="category-select_sortBy"
             class="navbar__select"
-            value=""
             @input="changeSorting"
           >
             <SfSelectOption
