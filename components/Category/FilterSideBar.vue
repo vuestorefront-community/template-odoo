ab6dcdd904c5cd9c137876c911e88f517ea18380
ekhan <ekhan@odoogap.com>
2023-06-07 17:02:53 +0600

    16043

--- components/Category/FilterSideBar.vue
+++ components/Category/FilterSideBar.vue
@@ -7,7 +7,6 @@
   >
     <div class="filters desktop-only">
       <div v-for="(facet, i) in facets" :key="i">
-        <!-- {{  facet }} -->
         <template v-if="isFacetPrice(facet)">
           <SfHeading
             :level="4"
@@ -84,12 +83,12 @@
     <template #content-bottom>
       <div class="filters__buttons">
         <SfButton class="sf-button--full-width" @click="applyFilters">{{
-          $t("Done")
+          $t('Done')
         }}</SfButton>
         <SfButton
           class="sf-button--full-width filters__button-clear"
           @click="clearFilters"
-          >{{ $t("Clear all") }}</SfButton
+          >{{ $t('Clear all') }}</SfButton
         >
       </div>
     </template>
@@ -108,17 +107,17 @@ import {
   SfProperty,
   SfImage,
   SfRange,
-  SfAccordion,
-} from "@storefront-ui/vue";
-import { facetGetters } from "@vue-storefront/odoo";
+  SfAccordion
+} from '@storefront-ui/vue';
+import { facetGetters } from '@vue-storefront/odoo';
 import {
   defineComponent,
   ref,
   onMounted,
   reactive,
-  computed,
-} from "@nuxtjs/composition-api";
-import { useUiState, useUiHelpers } from "~/composables";
+  computed
+} from '@nuxtjs/composition-api';
+import { useUiState, useUiHelpers } from '~/composables';
 
 export default defineComponent({
   components: {
@@ -133,13 +132,13 @@ export default defineComponent({
     SfProperty,
     SfAccordion,
     SfImage,
-    SfRange,
+    SfRange
   },
   props: {
     facetsList: {
       type: Object,
-      default: () => [],
-    },
+      default: () => []
+    }
   },
   setup(props) {
     const selectedFilters = ref([]);
@@ -149,11 +148,11 @@ export default defineComponent({
       range: { min: 20, max: 2000 },
       step: 10,
       connect: true,
-      direction: "ltr",
-      orientation: "horizontal",
-      behaviour: "tap-drag",
+      direction: 'ltr',
+      orientation: 'horizontal',
+      behaviour: 'tap-drag',
       tooltips: true,
-      keyboardSupport: true,
+      keyboardSupport: true
     });
 
     const { changeFilters, isFacetColor, isFacetPrice, facetsFromUrlToFilter } =
@@ -184,7 +183,7 @@ export default defineComponent({
       const newValue = `${values[0]}-${values[1]}`;
       price.value = values;
       const selectedValue = selectedFilters.value.find(
-        (item) => item?.filterName === "price"
+        (item) => item?.filterName === 'price'
       );
 
       if (selectedValue) {
@@ -193,9 +192,9 @@ export default defineComponent({
       }
 
       selectedFilters.value.push({
-        filterName: "price",
-        label: "Price",
-        id: newValue,
+        filterName: 'price',
+        label: 'Price',
+        id: newValue
       });
     };
 
@@ -203,12 +202,12 @@ export default defineComponent({
       const alreadySelectedIndex = selectedFilters.value.findIndex(
         (filter) => String(filter.id) === String(option.value)
       );
-
+      
       if (alreadySelectedIndex === -1) {
         selectedFilters.value.push({
           filterName: facet.label,
           label: option.label,
-          id: option.value,
+          id: option.value
         });
 
         return;
@@ -220,19 +219,19 @@ export default defineComponent({
     const facets = computed(() => [
       {
         id: null,
-        label: "Price",
-        type: "price",
+        label: 'Price',
+        type: 'price'
       },
-      ...facetGetters.getGrouped(props.facetsList, ["color", "size"]),
+      ...facetGetters.getGrouped(props.facetsList, ['color', 'size'])
     ]);
 
     const setPrice = () => {
       const selectedValue = selectedFilters.value.find(
-        (item) => item?.filterName === "price"
+        (item) => item?.filterName === 'price'
       );
-
+      
       if (selectedValue) {
-        const splitedPriceFromUrl = selectedValue?.id?.split("-");
+        const splitedPriceFromUrl = selectedValue?.id?.split('-');
 
         price.value = [splitedPriceFromUrl];
         config.start = splitedPriceFromUrl.map((item) => parseInt(item));
@@ -258,9 +257,9 @@ export default defineComponent({
       selectFilter,
       isFilterSelected,
       clearFilters,
-      applyFilters,
+      applyFilters
     };
-  },
+  }
 });
 </script>
 <style scoped lang="scss">
