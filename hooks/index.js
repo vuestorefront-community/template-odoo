127300ad01edeede7dc5c4bb642a3b7b2612c2be
Leonardo Muniz <lmuniz@odoogap.com>
2023-07-18 23:18:31 -0300

    Temporary comment add trailing slash to run ci

--- hooks/index.js
+++ hooks/index.js
@@ -7,7 +7,7 @@ export default {
   
   build: {
     async before(builder) {
-      await addTrailingSlashToUrls(process.argv);
+      //await addTrailingSlashToUrls(process.argv);
       if (!process.argv.includes("--dontGenerateCustomRoutes")) {
         await buildRoutes();
         await buildRedirects(builder);
