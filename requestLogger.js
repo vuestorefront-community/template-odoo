c089c5d8974b403e035fd3b9592f1e6352f7923a
inNetto <inetto.ferreira@gmail.com>
2023-08-07 19:10:07 -0300

    Refact requestLogger

--- requestLogger.js
+++ requestLogger.js
@@ -4,11 +4,11 @@ export default function (req, res, next) {
         console.log('----------------------------');
         console.log('----- REQUEST BEGINS. -----');
         console.log('----------------------------');
-        console.log('Método:', req.method);
+        console.log('Method:', req.method);
         console.log('URL:', req.url);
         
         const params = req.method === 'GET' ? req.query : req.body;
-        console.log('Parâmetros:', params);
+        console.log('Params:', params);
 
         console.log('-------------------------');
         console.log('----- REQUEST ENDS. -----');
