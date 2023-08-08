502239459d350a387c9c1ba236e4d9b914fa08f7
ekhan-odoogap <ekhan@odoogap.com>
2023-08-08 17:57:42 +0600

    16752

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Vue Storefront',
+    },
+  },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
 
   vite: {
