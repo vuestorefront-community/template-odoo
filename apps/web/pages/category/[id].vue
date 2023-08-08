9409ef73fef02f441a48949cabf16897876e6a5f
ekhan-odoogap <ekhan@odoogap.com>
2023-08-08 12:26:55 +0600

    16752

--- apps/web/pages/category/[id].vue
+++ apps/web/pages/category/[id].vue
@@ -1,43 +1,28 @@
 <script setup lang="ts">
-import { sdk } from '../../sdk.config';
 import { Product } from '@erpgap/odoo-sdk-api-client';
-
-const breadcrumbs: Breadcrumb[] = [
-  { name: 'home', link: '/' },
-  { name: 'allProducts', link: '/category' },
-];
+import { sdk } from '../../sdk.config';
 
 const products = useState<Product[]>('counter', () => []);
 
 if (products.value.length === 0) {
-  const { data } = await useAsyncData(
-    'counter',
-    async () =>
-      await sdk.odoo.getProductTemplateList({
-        pageSize: 12,
-        filter: { categoryId: [13] },
-      })
-  );
+  const { data } = await useAsyncData('counter', async () => await sdk.odoo.getProductTemplateList({ pageSize: 12, filter: { categoryId: [13] } }));
   products.value = data.value?.data.products?.products || [];
 }
-</script>
 
+</script>
 <template>
-    <CategoryPageContent
-      v-if="products"
-      title="All Products"
-      total-products="12"
-      :products="products"
-    >
-      <template #sidebar>
-        <CategoryTree
-          :categories="products"
-          :parent="{ name: 'All Products', href: '/' }"
-        />
-        <CategorySorting />
-        <CategoryFilters :facets="products" />
-      </template>
-    </CategoryPageContent>
+    <div>
+        <Breadcrumb class="self-start mb-10" />
+        <div class="flex flex-row items-stretch ">
+            <CategoryFilterSidebar class="mr-10" />
+
+            <div class="flex flex-row flex-wrap justify-between">
+                <ProductCard v-for="product in products" class="mb-4" :product="product" />
+            </div>
+        </div>
+    </div>
 </template>
 
-<style lang=""></style>
+<style lang="">
+
+</style>
