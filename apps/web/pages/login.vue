9d1742531407d059e6fece7eca58d7821c1e25ce
ekhan-odoogap <ekhan@odoogap.com>
2023-08-10 13:51:22 +0600

    16831

--- apps/web/pages/login.vue
+++ apps/web/pages/login.vue
@@ -30,61 +30,63 @@ const login = () => {
 </script>
 
 <template>
-  <NuxtLayout name="auth" :heading="$t('auth.login.heading')">
-    <form
-      @submit.prevent="login"
-      class="border-neutral-200 md:border flex flex-col gap-4 md:p-6 rounded-md"
-    >
-      <label>
-        <UiFormLabel>{{ $t('form.emailLabel') }}</UiFormLabel>
-        <SfInput
-          name="email"
-          type="email"
-          autocomplete="email"
-          v-model="email"
-          required
-        />
-      </label>
+  <div>
+    <NuxtLayout name="auth" :heading="$t('auth.login.heading')">
+      <form
+        @submit.prevent="login"
+        class="border-neutral-200 md:border flex flex-col gap-4 md:p-6 rounded-md"
+      >
+        <label>
+          <UiFormLabel>{{ $t('form.emailLabel') }}</UiFormLabel>
+          <SfInput
+            name="email"
+            type="email"
+            autocomplete="email"
+            v-model="email"
+            required
+          />
+        </label>
 
-      <label>
-        <UiFormLabel>{{ $t('form.passwordLabel') }}</UiFormLabel>
-        <FormPasswordInput
-          name="password"
-          autocomplete="current-password"
-          v-model="password"
-          required
-        />
-      </label>
+        <label>
+          <UiFormLabel>{{ $t('form.passwordLabel') }}</UiFormLabel>
+          <FormPasswordInput
+            name="password"
+            autocomplete="current-password"
+            v-model="password"
+            required
+          />
+        </label>
 
-      <label class="mt-2 flex items-center gap-2">
-        <SfCheckbox name="rememberMe" v-model="rememberMe" />
-        {{ $t('auth.login.rememberMeLabel') }}
-      </label>
+        <label class="mt-2 flex items-center gap-2">
+          <SfCheckbox name="rememberMe" v-model="rememberMe" />
+          {{ $t('auth.login.rememberMeLabel') }}
+        </label>
 
-      <SfButton type="submit" class="mt-2" :disabled="isLoading">
-        <SfLoaderCircular
-          v-if="isLoading"
-          class="flex justify-center items-center"
-          size="base"
-        />
-        <span v-else>
-          {{ $t('auth.login.submitLabel') }}
-        </span>
-      </SfButton>
-      <SfButton :tag="NuxtLink" to="/reset-password" variant="tertiary">
-        {{ $t('auth.login.forgotPasswordLabel') }}
-      </SfButton>
-    </form>
+        <SfButton type="submit" class="mt-2" :disabled="isLoading">
+          <SfLoaderCircular
+            v-if="isLoading"
+            class="flex justify-center items-center"
+            size="base"
+          />
+          <span v-else>
+            {{ $t('auth.login.submitLabel') }}
+          </span>
+        </SfButton>
+        <SfButton :tag="NuxtLink" to="/reset-password" variant="tertiary">
+          {{ $t('auth.login.forgotPasswordLabel') }}
+        </SfButton>
+      </form>
 
-    <UiAlert
-      class="mt-6 w-full p-4 md:p-6 !justify-start typography-text-base"
-      variant="neutral"
-    >
-      <i18n-t tag="span" keypath="auth.login.createAccountBanner">
-        <SfLink :tag="NuxtLink" to="/signup" variant="primary">
-          {{ $t('auth.login.createAccountLinkLabel') }}
-        </SfLink>
-      </i18n-t>
-    </UiAlert>
-  </NuxtLayout>
+      <UiAlert
+        class="mt-6 w-full p-4 md:p-6 !justify-start typography-text-base"
+        variant="neutral"
+      >
+        <i18n-t tag="span" keypath="auth.login.createAccountBanner">
+          <SfLink :tag="NuxtLink" to="/signup" variant="primary">
+            {{ $t('auth.login.createAccountLinkLabel') }}
+          </SfLink>
+        </i18n-t>
+      </UiAlert>
+    </NuxtLayout>
+  </div>
 </template>
