2674ef054f9cf0f84ac13210e04d3d40e697db25
ekhan-odoogap <ekhan@odoogap.com>
2023-08-16 18:07:07 +0600

    16753

--- apps/web/pages/category/[id].vue
+++ apps/web/pages/category/[id].vue
@@ -1,5 +1,6 @@
-<script setup>
-import { sdk } from '../../sdk.config';
+<script setup lang="ts">
+import { sdk } from '@/sdk.config';
+import { Product } from '@erpgap/odoo-sdk-api-client';
 import { SfButton, SfIconTune, useDisclosure } from '@storefront-ui/vue';
 import { useMediaQuery } from '@vueuse/core';
 
@@ -14,7 +15,7 @@ const breadcrumbs = [
 ];
 
 const route = useRoute();
-const products = ref([]);
+const products: any = ref([]);
 
 if (products.value.length === 0) {
   const { data } = await useAsyncData(
@@ -28,11 +29,11 @@ if (products.value.length === 0) {
   products.value = data.value?.data.products?.products || [];
 }
 
-const mountUrlSlugForProductVariant = (product) => {
+const mountUrlSlugForProductVariant = (product: { slug: any; variantAttributeValues: any; }) => {
   if (product) {
     const { slug, variantAttributeValues } = product;
     return `${slug}?${variantAttributeValues
-      .map((variant) => `${variant?.attribute?.name}=${variant?.id}&`)
+      .map((variant: { attribute: { name: any; }; id: any; }) => `${variant?.attribute?.name}=${variant?.id}&`)
       .join('')}`;
   }
 };
@@ -41,7 +42,7 @@ const isTabletScreen = useMediaQuery(mediaQueries.tablet);
 const isWideScreen = useMediaQuery(mediaQueries.desktop);
 const maxVisiblePages = ref(1);
 
-const setMaxVisiblePages = (isWide) => (maxVisiblePages.value = isWide ? 5 : 1);
+const setMaxVisiblePages = (isWide: boolean) => (maxVisiblePages.value = isWide ? 5 : 1);
 
 watch(isWideScreen, (value) => setMaxVisiblePages(value));
 
