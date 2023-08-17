96e361a1e96df12f53c9ecf1ff5387c492aaba97
ekhan-odoogap <ekhan@odoogap.com>
2023-08-17 19:36:56 +0600

    16834

--- apps/web/pages/checkout.vue
+++ apps/web/pages/checkout.vue
@@ -3,29 +3,87 @@ import {
   SfButton,
   SfIconArrowBack,
   SfLoaderCircular,
+  SfIconClose,
+  useDisclosure,
+  SfModal,
+  SfIconBlock,
+  SfListItem,
+  SfRadio,
+  SfLink,
 } from '@storefront-ui/vue';
 
+enum PaymentMethod {
+  CreditCard = 'credit-card',
+  PayPal = 'pay-pal',
+  ApplePay = 'apple-pay',
+  GooglePay = 'google-pay',
+}
+
 const NuxtLink = resolveComponent('NuxtLink');
-
-defineProps<{
-  backLabelDesktop: string;
-  backLabelMobile: string;
-  backHref: string;
-  heading: string;
-}>();
-
+const { isOpen, open, close } = useDisclosure();
+const cart = ref({
+  customerEmail: '',
+});
 const isLoading = false;
+const shippingAddress = ref({
+  firstName: 'Hieronim',
+  lastName: 'Anonim',
+  address1: 'Oak Drive',
+  address2: '3633',
+  city: 'Colonie',
+  country: 'US',
+  phoneNumber: '+1 321 765 0987',
+  postalCode: '12205',
+  state: 'NY',
+  titleCode: '',
+});
+const billingAddress = ref({
+  firstName: 'Hieronim',
+  lastName: 'Anonim',
+  address1: 'Oak Drive',
+  address2: '3633',
+  city: 'Colonie',
+  country: 'US',
+  phoneNumber: '+1 321 765 0987',
+  postalCode: '12205',
+  state: 'NY',
+  titleCode: '',
+});
+const shippingMethods = ref([
+  {
+    id: '1',
+    name: 'Standard',
+    estimatedDelivery: 'tomorrow',
+    price: '3',
+  },
+  {
+    id: '2',
+    name: 'Empress',
+    estimatedDelivery: 'tomorrow',
+    price: '10',
+  },
+]);
+const radioModel = ref('1');
+const activePayment = ref<PaymentMethod>(PaymentMethod.CreditCard);
+const data = ref({
+  length: 1,
+  totalPrice: '89.95',
+  subtotalRegularPrice: '100.99',
+  totalCouponDiscounts: '20',
+  shippingPrice: '0',
+  totalTax: '1.38',
+});
 </script>
 
 <template>
-  <div class="px-4 md:px-0 mb-20">
-    <div class="flex justify-between mt-8 mb-10 px-4 md:px-0">
+  <div class="md:px-0 mb-20">
+    <div class="flex justify-between mt-8 mb-10">
       <h1 class="font-bold typography-headline-3 md:typography-headline-2">
-        checkout
+        Checkout
       </h1>
       <SfButton
         :tag="NuxtLink"
-        :to="backHref"
+        to="/cart"
         class="flex md:hidden whitespace-nowrap"
         size="sm"
         variant="tertiary"
@@ -37,7 +95,7 @@ const isLoading = false;
       </SfButton>
       <SfButton
         :tag="NuxtLink"
-        :to="backHref"
+        to="/cart"
         class="hidden md:flex"
         variant="tertiary"
       >
@@ -51,60 +109,167 @@ const isLoading = false;
       <SfLoaderCircular size="3xl" />
     </span>
     <div v-else>
-      <div class="md:grid md:grid-cols-12 md:gap-x-6">
+      <div class="lg:grid lg:grid-cols-12 md:gap-x-6">
         <div class="col-span-7 mb-10 md:mb-0">
           <UiDivider class="w-screen md:w-auto -mx-4 md:mx-0" />
-          <ContactInformation />
+
+          <div data-testid="contact-information" class="md:px-4 py-6">
+            <div class="flex justify-between items-center">
+              <h2 class="text-neutral-900 text-lg font-bold mb-4">
+                {{ $t('contactInfo.heading') }}
+              </h2>
+              <SfButton
+                v-if="cart.customerEmail"
+                size="sm"
+                variant="tertiary"
+                @click="open"
+              >
+                {{ $t('contactInfo.edit') }}
+              </SfButton>
+            </div>
+            <div v-if="cart.customerEmail" class="mt-2 md:w-[520px]">
+              <p>{{ cart.customerEmail }}</p>
+            </div>
+            <div v-else class="w-full md:max-w-[520px]">
+              <p>{{ $t('contactInfo.description') }}</p>
+              <SfButton
+                class="mt-4 w-full md:w-auto"
+                variant="secondary"
+                @click="open"
+              >
+                {{ $t('contactInfo.add') }}
+              </SfButton>
+            </div>
+
+            <SfModal
+              v-model="isOpen"
+              tag="section"
+              role="dialog"
+              class="h-full w-full overflow-auto md:w-[600px] md:h-fit z-50"
+              aria-labelledby="contact-modal-title"
+            >
+              <header>
+                <SfButton
+                  square
+                  variant="tertiary"
+                  class="absolute right-2 top-2"
+                  @click="close"
+                >
+                  <SfIconClose />
+                </SfButton>
+                <h3
+                  id="contact-modal-title"
+                  class="text-neutral-900 text-lg md:text-2xl font-bold mb-4"
+                >
+                  {{ $t('contactInfo.heading') }}
+                </h3>
+              </header>
+              <FormContactInformation on-save="close" @on-cancel="close" />
+            </SfModal>
+          </div>
+
           <UiDivider class="w-screen md:w-auto -mx-4 md:mx-0" />
-          <CheckoutAddress
+          <CheckoutAddressForm
             :heading="$t('billing.heading')"
             :description="$t('billing.description')"
             :button-text="$t('billing.addButton')"
-            :saved-address="cart.billingAddress"
+            :saved-address="billingAddress"
             type="billingAddress"
           />
           <UiDivider class="w-screen md:w-auto -mx-4 md:mx-0" />
-          <CheckoutAddress
+          <CheckoutAddressForm
             :heading="$t('shipping.heading')"
             :description="$t('shipping.description')"
             :button-text="$t('shipping.addButton')"
-            :saved-address="cart.shippingAddress"
+            :saved-address="shippingAddress"
             type="shippingAddress"
           />
+
           <UiDivider class-name="w-screen md:w-auto -mx-4 md:mx-0" />
-          <ShippingMethod :shipping-methods="shippingMethods" />
+          <div data-testid="shipping-method" class="md:px-4 my-6">
+            <div class="flex justify-between items-center">
+              <h3 class="text-neutral-900 text-lg font-bold">
+                {{ $t('shippingMethod.heading') }}
+              </h3>
+            </div>
+            <div class="mt-4">
+              <ul
+                v-if="shippingMethods"
+                class="grid gap-y-4 md:grid-cols-2 md:gap-x-4"
+                role="radiogroup"
+              >
+                <SfListItem
+                  v-for="{
+                    id,
+                    name,
+                    estimatedDelivery,
+                    price,
+                  } in shippingMethods"
+                  tag="label"
+                  :key="id"
+                  class="border rounded-md items-start"
+                  @click="radioModel = id"
+                >
+                  <div class="flex gap-2">
+                    <SfRadio v-model="radioModel" checked="1" :value="id" />
+                    <div>
+                      <p>{{ name }}</p>
+                      <p class="text-xs text-neutral-500">
+                        {{ estimatedDelivery }}
+                      </p>
+                    </div>
+                    <p class="ml-auto">${{ price }}</p>
+                  </div>
+                </SfListItem>
+              </ul>
+
+              <div v-else class="flex mb-6">
+                <SfIconBlock class="mr-2 text-neutral-500" />
+                <p>{{ $t('shippingMethod.description') }}</p>
+              </div>
+            </div>
+          </div>
           <UiDivider class="w-screen md:w-auto -mx-4 md:mx-0" />
-          <CheckoutPayment
+          <CheckoutPaymentMethod
             :active-payment="activePayment"
             @update:active-payment="activePayment = $event"
           />
           <UiDivider class="w-screen md:w-auto -mx-4 md:mx-0 mb-10" />
         </div>
-        <!-- <OrderSummary v-if="cart" :cart="cart" class="col-span-5 md:sticky md:top-20 h-fit">
-        <SfButton :tag="NuxtLink" :to="paths.orderSuccess" size="lg" class="w-full mb-4 md:mb-0">
-          {{ $t('placeOrder') }}
-        </SfButton>
-        <p class="text-sm text-center mt-4 pb-4 md:pb-0">
-          <i18n-t keypath="termsInfo">
-            <template #terms>
-              <SfLink
-                href="#"
-                class="focus:outline focus:outline-offset-2 focus:outline-2 outline-secondary-600 rounded"
-              >
-                {{ $t('termsAndConditions') }}
-              </SfLink>
-            </template>
-            <template #privacyPolicy>
-              <SfLink
-                href="#"
-                class="focus:outline focus:outline-offset-2 focus:outline-2 outline-secondary-600 rounded"
-              >
-                {{ $t('privacyPolicy') }}
-              </SfLink>
-            </template>
-          </i18n-t>
-        </p>
-      </OrderSummary> -->
+        <UiOrderSummary
+          v-if="data"
+          :cart="data"
+          class="col-span-5 md:sticky md:top-20 h-fit"
+        >
+          <SfButton
+            :tag="NuxtLink"
+            to="/checkout"
+            size="lg"
+            class="w-full mb-4 md:mb-0"
+          >
+            {{ $t('placeOrder') }}
+          </SfButton>
+          <p class="text-sm text-center mt-4 pb-4 md:pb-0">
+            <i18n-t keypath="termsInfo">
+              <template #terms>
+                <SfLink
+                  href="#"
+                  class="focus:outline focus:outline-offset-2 focus:outline-2 outline-secondary-600 rounded"
+                >
+                  {{ $t('termsAndConditions') }}
+                </SfLink>
+              </template>
+              <template #privacyPolicy>
+                <SfLink
+                  href="#"
+                  class="focus:outline focus:outline-offset-2 focus:outline-2 outline-secondary-600 rounded"
+                >
+                  {{ $t('privacyPolicy') }}
+                </SfLink>
+              </template>
+            </i18n-t>
+          </p>
+        </UiOrderSummary>
       </div>
     </div>
   </div>
