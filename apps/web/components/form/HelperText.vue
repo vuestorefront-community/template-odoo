4c186cd59587f79210fad4a98e70d89e03a05342
ekhan-odoogap <ekhan@odoogap.com>
2023-08-17 19:40:42 +0600

    16834

--- apps/web/components/form/HelperText.vue
+++ apps/web/components/form/HelperText.vue
@@ -1,5 +1,8 @@
 <template>
-  <small class="text-sm text-neutral-500 mt-0.5 block" data-testid="form-helper-text">
+  <small
+    class="text-sm text-neutral-500 mt-0.5 block"
+    data-testid="form-helper-text"
+  >
     <slot />
   </small>
 </template>
