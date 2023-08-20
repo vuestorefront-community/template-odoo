4417811e0e49757a7dd5a85f7d1708db2686b98d
ekhan-odoogap <ekhan@odoogap.com>
2023-08-21 03:06:58 +0600

    16753

--- apps/web/composables/useCategory/index.ts
+++ apps/web/composables/useCategory/index.ts
@@ -65,6 +65,6 @@ export const useCategory: any = () => {
     loading,
     loadCategoryProducts,
     loadCategoryList,
-    loadCategory
+    loadCategory,
   };
 };
--- apps/web/composables/useUiHelpers/index.ts
+++ apps/web/composables/useUiHelpers/index.ts
@@ -1,3 +1,201 @@
+import { facets } from './facts';
+
 export const useUiHelpers: any = () => {
-  return {};
+  const route: any = useRoute();
+  const router = useRouter();
+  const { query, path } = route;
+
+  const queryParamsNotFilters = ['page', 'sort', 'itemsPerPage'];
+  const localePrefixes = ['/en', '/de', '/ru'];
+  const pathToSlug = (): string => {
+    for (const localePrefix of localePrefixes) {
+      if (path.startsWith(localePrefix)) {
+        return path.replace(localePrefix, '');
+      }
+    }
+    return path;
+  };
+  const getGroups = (searchData: any) => {
+    if (!searchData) return [];
+
+    const data: any = [];
+
+    searchData.forEach((item: any) => {
+      const current = data.find(
+        (itemData: { attributeName: any }) =>
+          itemData.attributeName === item.attribute?.name
+      );
+
+      if (!current) {
+        data.push({
+          id: String(item.attribute.id),
+          label: item.attribute?.name,
+          attributeName: item.attribute?.name,
+          type: item.displayType,
+          count: 0,
+          options: [],
+        });
+      }
+
+      data
+        .find(
+          (itemData: { attributeName: any }) =>
+            itemData.attributeName === item.attribute?.name
+        )
+        .options.push({
+          id: String(item.search),
+          value: item.id,
+          label: item.name,
+          metadata: item.search,
+          htmlColor: item.htmlColor,
+        });
+    });
+    return data;
+  };
+
+  const getAttributeValues = (filterKey: string, value: string) => {
+    const attribute: any = facets.find((item) => {
+      return item.label === filterKey;
+    });
+    let option = {};
+    if (attribute) {
+      option = attribute?.options.find((item: { value: any }) => {
+        return Number(item.value) === Number(value.split('-')[0]);
+      });
+    }
+    return option;
+  };
+
+  const getFacetsFromURL = (): any => {
+    const filters: string[] = [];
+    if (query) {
+      Object.keys(query).forEach((filterKey) => {
+        if (![...queryParamsNotFilters, 'price'].includes(filterKey)) {
+          if (query[filterKey].includes(',')) {
+            query[filterKey]?.split(',').forEach((label: string) => {
+              const getProperAttribute: any = getAttributeValues(
+                filterKey,
+                label?.split('-')[0]
+              );
+              filters.push(getProperAttribute?.id);
+            });
+          } else {
+            const label = query[filterKey]?.split(',')[0];
+            const getProperAttribute: any = getAttributeValues(
+              filterKey,
+              label
+            );
+            filters.push(getProperAttribute?.id);
+          }
+        }
+      });
+    }
+
+    const price = query?.price?.split('-');
+
+    const pageSize = query.itemsPerPage ? parseInt(query.itemsPerPage) : 12;
+    const sort = query?.sort?.split(',') || [];
+    const page = query?.page || 1;
+
+    const productFilters = {
+      minPrice: Number(price?.[0]) || null,
+      maxPrice: Number(price?.[1]) || null,
+      attribValues: filters,
+      categorySlug: path === '/' ? null : pathToSlug(),
+    };
+    const filtersForHash = {
+      ...productFilters,
+      pageSize,
+      price,
+      page,
+      sort,
+    };
+
+    return {
+      pageSize,
+      currentPage: parseInt(page),
+      // cacheKey: `API-P${hash(filtersForHash, { algorithm: 'md5' })}`,
+      search: '',
+      sort: { [sort[0]]: sort[1] },
+      filter: productFilters,
+    };
+  };
+  const facetsFromUrlToFilter = () => {
+    const formattedFilters: any = [];
+    Object.keys(query).forEach((label) => {
+      if (queryParamsNotFilters.includes(label)) return;
+
+      const valueList = query[label].split(',');
+
+      valueList.forEach((value: any) => {
+        if (label === 'price') {
+          const item = {
+            filterName: label,
+            label: `${value.slice(0, 2)}`,
+            id: value,
+          };
+          formattedFilters.push(item);
+        } else {
+          const newVal = value?.split('-');
+          const item = {
+            filterName: label,
+            label: `${newVal[1] ?? newVal[0]}`,
+            id: `${newVal[0]}`,
+          };
+          formattedFilters.push(item);
+        }
+      });
+    });
+
+    return formattedFilters;
+  };
+
+  const changeFilters = (filters: any[]) => {
+    const formattedFilters: any = {};
+    filters.forEach((element) => {
+      if (element.filterName === 'Size') {
+        if (formattedFilters[element.filterName]) {
+          formattedFilters[
+            element.filterName
+          ] += `,${element.id}-${element.label}`;
+          return;
+        }
+        formattedFilters[element.filterName] = `${element.id}-${element.label}`;
+      } else {
+        if (formattedFilters[element.filterName]) {
+          formattedFilters[element.filterName] += `,${element.id}`;
+          return;
+        }
+        formattedFilters[element.filterName] = `${element.id}`;
+      }
+    });
+
+    let allQuery: any = {};
+    if (filters.length > 0) {
+      allQuery = { ...formattedFilters };
+    } else {
+      allQuery = { ...formattedFilters };
+      if (query.itemsPerPage) {
+        allQuery = { itemsPerPage: query.itemsPerPage };
+      }
+    }
+
+    if (query.sort) {
+      allQuery.sort = query.sort;
+    }
+
+    delete allQuery.page;
+
+    router.push({ query: allQuery });
+  };
+  const clearAllFilters = () => {
+    router.push({ query: {} });
+  };
+  return {
+    getGroups,
+    getFacetsFromURL,
+    changeFilters,
+    clearAllFilters,
+    facetsFromUrlToFilter,
+  };
 };
