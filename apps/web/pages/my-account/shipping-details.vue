d79a83406dae501cac79bed5e44164b3a321ef21
ekhan-odoogap <ekhan@odoogap.com>
2023-08-23 23:07:25 +0600

    16753

---
+++ apps/web/pages/my-account/shipping-details.vue
@@ -0,0 +1,79 @@
+<template>
+  <UiDivider class="w-screen -mx-4 md:col-span-3 md:w-auto md:mx-0" />
+  <AccountProfileData
+    class="col-span-3"
+    :header="$t('account.accountSettings.shippingDetails.shippingAddress')"
+    :button-text="$t('account.accountSettings.personalData.edit')"
+    @on-click="open"
+  >
+    <p>
+      {{ userShippingAddress.firstName }} {{ userShippingAddress.lastName }}
+    </p>
+    <p>{{ userShippingAddress.phoneNumber }}</p>
+    <p>{{ userShippingAddress.address1 }} {{ userShippingAddress.address2 }}</p>
+    <p>
+      {{ userShippingAddress.city }}, {{ userShippingAddress.state }}
+      {{ userShippingAddress.postalCode }}
+    </p>
+  </AccountProfileData>
+  <UiDivider class="w-screen -mx-4 md:col-span-3 md:w-auto md:mx-0" />
+  <UiOverlay v-if="isOpen" :visible="isOpen">
+    <SfModal
+      v-model="isOpen"
+      tag="section"
+      role="dialog"
+      class="h-full w-full overflow-auto md:w-[600px] md:h-fit"
+      aria-labelledby="address-modal-title"
+    >
+      <header>
+        <SfButton
+          square
+          variant="tertiary"
+          class="absolute right-2 top-2"
+          @click="close"
+        >
+          <SfIconClose />
+        </SfButton>
+        <h3
+          id="address-modal-title"
+          class="text-neutral-900 text-lg md:text-2xl font-bold mb-4"
+        >
+          {{ $t('account.accountSettings.shippingDetails.shippingAddress') }}
+        </h3>
+      </header>
+      <FormAddAddress
+        :saved-address="userShippingAddress"
+        type="shippingAddress"
+        @on-save="close"
+        @on-close="close"
+      />
+    </SfModal>
+  </UiOverlay>
+</template>
+
+<script setup lang="ts">
+import {
+  SfButton,
+  SfIconClose,
+  SfModal,
+  useDisclosure,
+} from '@storefront-ui/vue';
+
+definePageMeta({
+  layout: 'account',
+});
+const { isOpen, open, close } = useDisclosure();
+
+const userShippingAddress = ref({
+  firstName: 'Hieronim',
+  lastName: 'Anonim',
+  address1: 'Oak Drive',
+  address2: '3633',
+  city: 'Colonie',
+  country: 'US',
+  phoneNumber: '+1 321 765 0987',
+  postalCode: '12205',
+  state: 'NY',
+  titleCode: '',
+});
+</script>
