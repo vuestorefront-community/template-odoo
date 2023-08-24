af457af267ad3f30343372063ae7d1237882fb2a
inNetto <inetto.ferreira@gmail.com>
2023-08-24 19:54:31 -0300

    feat(cartAdd composable and usages): adding useCart in product slug and category list product page
    cartAdd available to use in files

--- apps/web/composables/index.ts
+++ apps/web/composables/index.ts
@@ -2,5 +2,6 @@ import { useCategory } from './useCategory';
 import { useProduct } from './useProduct';
 import { useProductAttributes } from './useProductAttributes';
 import { useUiHelpers } from './useUiHelpers';
+import { useCart } from './useCart';
 
-export { useCategory, useProduct, useProductAttributes, useUiHelpers };
+export { useCategory, useProduct, useProductAttributes, useCart, useUiHelpers };
